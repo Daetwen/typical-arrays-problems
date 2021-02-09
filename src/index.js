329db18ac664f120a5afb6923d128a71bce642ad
Daetwen <62154512+Daetwen@users.noreply.github.com>
2021-02-09 14:55:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
+
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 1){
+    let min = array[0];
+    for(let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+      min = array[i];
+      }
+    }
+    return min;
+  }
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0){
+      let max = array[0];
+      for(let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+        max = array[i];
+        }
+      }
+      return max;
+    }
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let all = 0;
+  let size = 0;
+  if (array && array.length > 0) {
+    for(let i = 0; i < array.length; i++) {
+      all+=array[i];
+        size++;
+    }
+    return all/size;
+  }
+  else return 0;
+
 }
